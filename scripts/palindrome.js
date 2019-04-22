16f0d8a392ab603f106674c23bbc0bd9fda8250f
Claudia Vizena <themodernlover@yahoo.com>
2019-04-21 18:30:37 -0700

    create foreach, palindrome, and function documents

--- scripts/palindrome.js
+++ scripts/palindrome.js
@@ -1,4 +1,10 @@
 // Reverses a string.
 function reverse(string) {
-  return string.split("").reverse().join("");
+  return Array.from(string).reverse().join("")
+}
+
+// Returns true for a palindrome, false otherwise.
+function palindrome(string) {
+  let processedContent = string.toLowerCase();
+  return processedContent === reverse(processedContent);
 }
