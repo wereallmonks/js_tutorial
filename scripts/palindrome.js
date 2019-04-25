ecba2e0a929689ab1eda513e48d282a2bb2fb797
Claudia Vizena <themodernlover@yahoo.com>
2019-04-25 12:01:18 -0700

    assign a function directly to the String.prototype

--- scripts/palindrome.js
+++ scripts/palindrome.js
@@ -1,27 +1,19 @@
-// Reverses a string.
-/*function reverse(string) {
-  return Array.from(string).reverse().join("")
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
-*/
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
   }
 
-  // Makes the phrase LOUDER.
-  this.louder = function louder() {
-    let yell = this.content.toUpperCase();
-    return yell;
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
